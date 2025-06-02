8b07ec828582328cdb681e59e72222802fb82b49
Magesh Kuppan <tkmagesh77@gmail.com>
2025-06-02 15:17:43 +0530

    updates & bug fixes

--- 05-functions/02-assignment.go
+++ 05-functions/02-assignment.go
@@ -9,13 +9,9 @@ import "fmt"
 func main() {
 	var userChoice, n1, n2 int
 	for {
-		getUserChoice(&userChoice)
+		userChoice = getUserChoice()
 		if userChoice == 5 {
 			break
-		}
-		if userChoice < 1 || userChoice > 5 {
-			fmt.Println("Invalid Choice.. try again!")
-			continue
 		}
 		getOperands(&n1, &n2)
 		processChoice(userChoice, n1, n2)
@@ -38,19 +34,27 @@ func processChoice(userChoice, n1, n2 int) {
 	fmt.Println("Result :", result)
 }
 
-func getUserChoice(userChoice *int) {
-	fmt.Println("1. Add")
-	fmt.Println("2. Subtract")
-	fmt.Println("3. Multiply")
-	fmt.Println("4. Divide")
-	fmt.Println("5. Exit")
-	fmt.Println("Enter your choice:")
-	fmt.Scanln(userChoice)
+func getUserChoice() int {
+	var userChoice int
+	for {
+		fmt.Println("1. Add")
+		fmt.Println("2. Subtract")
+		fmt.Println("3. Multiply")
+		fmt.Println("4. Divide")
+		fmt.Println("5. Exit")
+		fmt.Println("Enter your choice:")
+		fmt.Scanln(&userChoice)
+		if userChoice < 1 || userChoice > 5 {
+			fmt.Println("Invalid Choice.. try again!")
+			continue
+		}
+		return userChoice
+	}
 }
 
 func getOperands(n1, n2 *int) {
 	fmt.Println("Enter the operands :")
-	fmt.Scanln(&n1, &n2)
+	fmt.Scanln(n1, n2)
 }
 
 func add(x, y int) int {
