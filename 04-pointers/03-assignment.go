06ce4b2fbc8421ccee472d690e989b1fdfdd37ed
Magesh Kuppan <tkmagesh77@gmail.com>
2025-06-02 14:33:50 +0530

    pointers - assignment - solution

--- 04-pointers/03-assignment.go
+++ 04-pointers/03-assignment.go
@@ -5,8 +5,11 @@ package main
 import "fmt"
 
 func main() {
+	var start, end int
+	fmt.Println("Enter the range :")
+	fmt.Scanln(&start, &end)
 LOOP:
-	for no := 2; no <= 100; no++ {
+	for no := start; no <= end; no++ {
 		for i := 2; i <= (no / 2); i++ {
 			if no%i == 0 {
 				continue LOOP
