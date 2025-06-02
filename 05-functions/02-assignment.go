8109e7db9f8bf5a2937e91ba1dfd648a390af854
Magesh Kuppan <tkmagesh77@gmail.com>
2025-06-02 15:13:10 +0530

    functions - assignment - solution

--- 05-functions/02-assignment.go
+++ 05-functions/02-assignment.go
@@ -7,16 +7,9 @@ package main
 import "fmt"
 
 func main() {
-	var userChoice, n1, n2, result int
+	var userChoice, n1, n2 int
 	for {
-
-		fmt.Println("1. Add")
-		fmt.Println("2. Subtract")
-		fmt.Println("3. Multiply")
-		fmt.Println("4. Divide")
-		fmt.Println("5. Exit")
-		fmt.Println("Enter your choice:")
-		fmt.Scanln(&userChoice)
+		getUserChoice(&userChoice)
 		if userChoice == 5 {
 			break
 		}
@@ -24,19 +17,54 @@ func main() {
 			fmt.Println("Invalid Choice.. try again!")
 			continue
 		}
-		fmt.Println("Enter the operands :")
-		fmt.Scanln(&n1, &n2)
-		switch userChoice {
-		case 1:
-			result = n1 + n2
-		case 2:
-			result = n1 - n2
-		case 3:
-			result = n1 * n2
-		case 4:
-			result = n1 / n2
-		}
-		fmt.Println("Result :", result)
+		getOperands(&n1, &n2)
+		processChoice(userChoice, n1, n2)
 	}
 	fmt.Println("Thank you!")
+}
+
+func processChoice(userChoice, n1, n2 int) {
+	var result int
+	switch userChoice {
+	case 1:
+		result = add(n1, n2)
+	case 2:
+		result = subtract(n1, n2)
+	case 3:
+		result = multply(n1, n2)
+	case 4:
+		result = divide(n1, n2)
+	}
+	fmt.Println("Result :", result)
+}
+
+func getUserChoice(userChoice *int) {
+	fmt.Println("1. Add")
+	fmt.Println("2. Subtract")
+	fmt.Println("3. Multiply")
+	fmt.Println("4. Divide")
+	fmt.Println("5. Exit")
+	fmt.Println("Enter your choice:")
+	fmt.Scanln(userChoice)
+}
+
+func getOperands(n1, n2 *int) {
+	fmt.Println("Enter the operands :")
+	fmt.Scanln(&n1, &n2)
+}
+
+func add(x, y int) int {
+	return x + y
+}
+
+func subtract(x, y int) int {
+	return x - y
+}
+
+func multply(x, y int) int {
+	return x * y
+}
+
+func divide(x, y int) int {
+	return x / y
 }
