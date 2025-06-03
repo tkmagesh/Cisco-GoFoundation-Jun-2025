27691513a84127f18a0428b6efb32ead40008dcf
Magesh Kuppan <tkmagesh77@gmail.com>
2025-06-03 11:28:05 +0530

    collections - assingment - solution

--- 06-collections/05-assignment.go
+++ 06-collections/05-assignment.go
@@ -13,13 +13,30 @@ package main
 import "fmt"
 
 func main() {
-LOOP:
-	for no := 2; no <= 100; no++ {
-		for i := 2; i <= (no / 2); i++ {
-			if no%i == 0 {
-				continue LOOP
-			}
-		}
-		fmt.Println("Prime No :", no)
+	var start, end int
+	fmt.Println("Enter the range :")
+	fmt.Scanln(&start, &end)
+	primes := generatePrimes(start, end)
+	for _, primeNo := range primes {
+		fmt.Printf("Prime No : %d\n", primeNo)
 	}
+}
+
+func generatePrimes(start, end int) []int {
+	primes := make([]int, 0)
+	for no := start; no <= end; no++ {
+		if isPrime(no) {
+			primes = append(primes, no)
+		}
+	}
+	return primes
+}
+
+func isPrime(no int) bool {
+	for i := 2; i <= (no / 2); i++ {
+		if no%i == 0 {
+			return false
+		}
+	}
+	return true
 }
