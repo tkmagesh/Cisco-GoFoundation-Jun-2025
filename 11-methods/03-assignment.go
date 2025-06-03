278e99a2c7f466291935f0791b1997a98f741811
Magesh Kuppan <tkmagesh77@gmail.com>
2025-06-03 16:28:23 +0530

    Day-02 homework

---
+++ 11-methods/03-assignment.go
@@ -0,0 +1,19 @@
+/*
+Create the types to simulate a shopping cart
+
+Cart
+	-> Collection of LineItems
+
+LineItem
+	-> Created for a product
+	-> units
+
+Product
+	-> Id, Name, Cost
+
+Functinality
+	Create a ShoppingCart instance
+	Add line items
+	Print all the products & their respective units for each line item
+	Print the Cart Value (sum(product cost * units))
+*/
