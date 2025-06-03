4499b84844ad6c0351f20a21bf41b44c4e903a03
Magesh Kuppan <tkmagesh77@gmail.com>
2025-06-03 15:50:42 +0530

    type definition for structs

--- 10-structs/02-typed.go
+++ 10-structs/02-typed.go
@@ -3,13 +3,15 @@ package main
 
 import "fmt"
 
+type Product struct {
+	Id   int
+	Name string
+	Cost float64
+}
+
 func main() {
 	/*
-		var p struct {
-			Id   int
-			Name string
-			Cost float64
-		}
+		var p Product
 
 		// accessing the attributes (using "." notation)
 		p.Id = 100
@@ -17,15 +19,7 @@ func main() {
 		p.Cost = 10
 	*/
 
-	var p struct {
-		Id   int
-		Name string
-		Cost float64
-	} = struct {
-		Id   int
-		Name string
-		Cost float64
-	}{
+	var p Product = Product{
 		Id:   100,
 		Name: "Pen",
 		Cost: 10,
@@ -35,31 +29,19 @@ func main() {
 	// fmt.Printf("%#v\n", p)
 	fmt.Printf("%+v\n", p)
 
-	var p2 struct {
-		Id   int
-		Name string
-		Cost float64
-	}
+	var p2 Product
 	p2 = p // a copy is created coz 'struct instances are just values'
 	p2.Cost = 12
 	fmt.Printf("p2.Cost = %0.2f, p.Cost = %0.2f\n", p2.Cost, p.Cost)
 
 	// structs are compared by values
-	var x = struct {
-		Id   int
-		Name string
-		Cost float64
-	}{
+	var x = Product{
 		Id:   200,
 		Name: "Pencil",
 		Cost: 5,
 	}
 
-	var y = struct {
-		Id   int
-		Name string
-		Cost float64
-	}{
+	var y = Product{
 		Id:   200,
 		Name: "Pencil",
 		Cost: 5,
@@ -68,11 +50,7 @@ func main() {
 	fmt.Println("x == y :", x == y)
 
 	// Handling references
-	var pPtr *struct {
-		Id   int
-		Name string
-		Cost float64
-	}
+	var pPtr *Product
 	pPtr = &p
 
 	// no need to deference to access the members through a pointer
@@ -91,18 +69,10 @@ func main() {
 
 */
 
-func Format(p struct {
-	Id   int
-	Name string
-	Cost float64
-}) string {
+func Format(p Product) string {
 	return fmt.Sprintf("Id = %d, Name = %q, Cost = %0.2f", p.Id, p.Name, p.Cost)
 }
 
-func ApplyDiscount(pPtr *struct {
-	Id   int
-	Name string
-	Cost float64
-}, discountPercentage float64) {
+func ApplyDiscount(pPtr *Product, discountPercentage float64) {
 	pPtr.Cost = pPtr.Cost * ((100 - discountPercentage) / 100)
 }
