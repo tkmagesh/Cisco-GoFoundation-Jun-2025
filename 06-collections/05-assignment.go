582f3db07267af2827daeb770ba7016f7b0fe99c
Magesh Kuppan <tkmagesh77@gmail.com>
2025-06-03 11:07:58 +0530

    updates

--- 06-collections/05-assignment.go
+++ 06-collections/05-assignment.go
@@ -1,5 +1,6 @@
 /*
-Refactor the logic for generating prime nos to a "generatePrimes" function
+Refactor the logic for generating prime nos to a "generatePrimes()" function
+Write "IsPrime()" that checks if the given number is a prime number or not
 Print the generated prime numbers in the main() function
 */
 
