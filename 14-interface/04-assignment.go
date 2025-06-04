8460942cbc7ff4c6468940815d3def2e2054cef9
Magesh Kuppan <tkmagesh77@gmail.com>
2025-06-04 11:32:07 +0530

    assignment

---
+++ 14-interface/04-assignment.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Product struct {
+	Id       int
+	Name     string
+	Cost     float32
+	Units    int
+	Category string
+}
+
+// fmt.Stringer interface implementation
+func (product Product) String() string {
+	return fmt.Sprintf("Id = %d, Name = %q, Cost = %.2f, Units = %d, Category = %q", product.Id, product.Name, product.Cost, product.Units, product.Category)
+}
+
+/*
+Create a "Sort" api to sort the products by "any attribute"
+	IMPORTANT: DO NOT write your own implementation for sorting. Instead MUST use sort.Sort() function
+*/
+
+func main() {
+	products := []Product{
+		Product{105, "Pen", 5, 50, "Stationary"},
+		Product{107, "Pencil", 2, 100, "Stationary"},
+		Product{103, "Marker", 50, 20, "Utencil"},
+		Product{102, "Stove", 5000, 5, "Utencil"},
+		Product{101, "Kettle", 2500, 10, "Utencil"},
+		Product{104, "Scribble Pad", 20, 20, "Stationary"},
+		Product{109, "Golden Pen", 2000, 20, "Stationary"},
+	}
+}
