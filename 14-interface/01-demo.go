1413c9554193169596fd60bc5b35e4dad9919efa
Magesh Kuppan <tkmagesh77@gmail.com>
2025-06-04 11:09:41 +0530

    interface composition

--- 14-interface/01-demo.go
+++ 14-interface/01-demo.go
@@ -68,18 +68,53 @@ func (s Square) Area() float64 {
 // Square : 4 * side
 
 // Make sure NONE of the above code is modified
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Height + r.Width)
+}
+
+func (s Square) Perimeter() float64 {
+	return 4 * s.Side
+}
+
+func PrintPerimeter(x interface{ Perimeter() float64 }) {
+	fmt.Println("Perimeter :", x.Perimeter())
+}
+
+// ver 5.0
+func PrintShape(x interface {
+	interface{ Area() float64 }
+	interface{ Perimeter() float64 }
+}) {
+	PrintArea(x)      // x should be interface{Area() float64}
+	PrintPerimeter(x) // x should be interface{ Perimeter() float64}
+}
 
 func main() {
 	c := Circle{Radius: 16}
 	// fmt.Println("Area :", c.Area())
-	PrintArea(c)
+	/*
+		PrintArea(c)
+		PrintPerimeter(c)
+	*/
+	PrintShape(c)
 
 	r := Rectangle{Height: 16, Width: 15}
 	// fmt.Println("Area :", r.Area())
-	PrintArea(r)
+	/*
+		PrintArea(r)
+		PrintPerimeter(r)
+	*/
+	PrintShape(r)
 
 	s := Square{Side: 22}
-	PrintArea(s)
+	/*
+		PrintArea(s)
+		PrintPerimeter(s)
+	*/
+	PrintShape(s)
 
-	PrintArea(100)
 }
