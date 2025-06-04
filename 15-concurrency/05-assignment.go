6d69ba515e1c38d9aa9a1d3381196263ffe0d062
Magesh Kuppan <tkmagesh77@gmail.com>
2025-06-04 16:51:16 +0530

    assignment solutions

--- 15-concurrency/05-assignment.go
+++ 15-concurrency/05-assignment.go
@@ -4,19 +4,29 @@ Make the execution of "checkPrime()" concurrent so that the number are not proce
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
 	var start, end int
 	fmt.Println("Enter the range :")
 	fmt.Scanln(&start, &end)
 	printPrimes(start, end)
+	fmt.Println("Done!")
 }
 
 func printPrimes(start, end int) {
+	wg := &sync.WaitGroup{}
 	for no := start; no <= end; no++ {
-		checkPrime(no)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			go checkPrime(no)
+		}()
 	}
+	wg.Wait()
 }
 
 func checkPrime(no int) {
--- 15-concurrency/12-assignment.go
+++ 15-concurrency/12-assignment.go
@@ -3,22 +3,38 @@ Modify the below:
 The generated prime number have to be printed in the main() function
 IMPORTANT: the primes numbers have to be printed AS AND WHEN they are generated
 */
-
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
 	var start, end int
+	ch := make(chan int)
 	fmt.Println("Enter the range :")
 	fmt.Scanln(&start, &end)
-	printPrimes(start, end)
+	go generatePrimes(start, end, ch)
+	for primeNo := range ch {
+		fmt.Println("Prime No :", primeNo)
+	}
+	fmt.Println("Done!")
 }
 
-func generatePrimes(start, end int) {
+func generatePrimes(start, end int, ch chan int) {
+	wg := &sync.WaitGroup{}
 	for no := start; no <= end; no++ {
-		checkPrime(no)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if checkPrime(no) {
+				ch <- no
+			}
+		}()
 	}
+	wg.Wait()
+	close(ch)
 }
 
 func checkPrime(no int) bool {
